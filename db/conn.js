2a692539fb4e567c716d7eb6fbfc89f7c71f21a6
FXNTGod <luizcarlosmatosmota@gmail.com>
2024-03-22 21:33:13 -0300

    aioo

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 
 async function main(){
-
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PWD}@cluster0.ae8ng2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+        await mongoose.connect(`mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PWD}@fxnt.uwvuhmw.mongodb.net/?retryWrites=true&w=majority&appName=FXNT`)
         console.log('Banco OK');
     } catch (error) {
         console.log('Erro: ' + error);
